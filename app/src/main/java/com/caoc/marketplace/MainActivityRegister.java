87f106299d34d38efbe72fdf886a42156ca23fda
caobandoc <obandocarlosandres@gmail.com>
2021-07-14 19:49:27 -0500

    Validaciones para la ventana de registro

--- app/src/main/java/com/caoc/marketplace/MainActivityRegister.java
+++ app/src/main/java/com/caoc/marketplace/MainActivityRegister.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import com.caoc.marketplace.ui.User;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivityRegister extends AppCompatActivity {
 
@@ -23,6 +28,8 @@ public class MainActivityRegister extends AppCompatActivity {
     private EditText et_phonenumber;
 
     private Button btn_register;
+
+    private CheckBox cb_term;
 
     private ArrayList<User> users;
 
@@ -37,7 +44,9 @@ public class MainActivityRegister extends AppCompatActivity {
         et_password = findViewById(R.id.et_password);
         et_phonenumber = findViewById(R.id.et_phonenumber);
 
-        btn_register = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        cb_term = findViewById(R.id.cb_term);
 
         Intent intent = getIntent();
         Bundle args = intent.getBundleExtra("BUNDLE");
@@ -51,17 +60,70 @@ public class MainActivityRegister extends AppCompatActivity {
         String password = et_password.getText().toString();
         String phonenumber = et_phonenumber.getText().toString();
 
-        User user = new User(names,surnames,email,password,phonenumber);
-
-        users.add(user);
-
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.txt_tittle_register);
-        builder.setMessage(R.string.txt_msg_register);
+        if(this.validate(names,surnames,email,password,phonenumber)){
+            User user = new User(names,surnames,email,password,phonenumber);
 
-        AlertDialog dialog = builder.create();
-        dialog.show();
+            users.add(user);
 
+            builder.setMessage(R.string.txt_msg_register);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }else{
+            builder.setMessage(R.string.txt_msg_fail);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+    }
+
+    public void accept(View v){
+        if(cb_term.isChecked() == true){
+            this.btn_register.setEnabled(true);
+        }else{
+            this.btn_register.setEnabled(false);
+        }
+    }
+
+    public boolean validate(String names, String surnames, String email, String password, String phonenumber){
+        boolean test = false;
+        if(names != null && surnames != null && email != null && password != null && phonenumber != null
+                && soloLetras(names) && soloLetras(surnames) && validarCorreo(email) && soloNumeros(phonenumber)){
+            test = true;
+        }
+        return test;
+    }
+
+    public boolean soloLetras(String s){
+        for (int x = 0; x < s.length(); x++) {
+            char c = s.charAt(x);
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validarCorreo(String s){
+        Pattern pattern = Pattern
+                .compile("([a-z0-9]+(\\.?[a-z0-9])*)+@(([a-z]+)\\.([a-z]+))+");
+
+        Matcher mather = pattern.matcher(s);
+
+        return mather.find();
+    }
+
+    public boolean soloNumeros(String s){
+        for (int x = 0; x < s.length(); x++) {
+            char c = s.charAt(x);
+            if (!(c >= '0' && c <= '9')) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
